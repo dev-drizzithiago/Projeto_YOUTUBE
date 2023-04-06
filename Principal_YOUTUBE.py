9519fa01af3a3ccddd03083a333367a3e592b6ad
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-04-06 06:39:23 -0300

    atualização

--- Principal_YOUTUBE.py
+++ Principal_YOUTUBE.py
@@ -1,11 +1,23 @@
 from pytube import YouTube
-import tkinter
+import tkinter as tk
+from tkinter import *
 
 
 class YouTube_v3:
     def __init__(self):
         print('BEM VINDO AO NOVO PROGRAMA!')
         print('Teste o novo programa!')
+        self.janela_principal_YT = tk.Tk()
 
+        self.frama_1 = tk.Frame(self.janela_principal_YT)
+        self.frama_1.pack()
+
+        self.frama_2 = tk.Frame(self.janela_principal_YT)
+        self.frama_2.pack()
+
+        arq_menu = Menu(self.janela_principal_YT)
+
+
+        tk.mainloop()
 
 iniciando = YouTube_v3()
