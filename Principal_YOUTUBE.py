7672d9811e0b6c78c5baeb3e35ec395d8e7db77f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-04-08 20:54:28 -0300

    atualizado

--- Principal_YOUTUBE.py
+++ Principal_YOUTUBE.py
@@ -1,32 +1,54 @@
 from pytube import YouTube
 import tkinter as tk
-from tkinter import *
+from tkinter import filedialog, Menu
 
 
 class YouTube_v3:
-    def nada_fazer(self):
-        novo_arq = Toplevel(self.janela_principal_YT)
-        botao_menu = Button(novo_arq, text="Botão fazer nada")
-        botao_menu.pack()
 
     def __init__(self):
         self.janela_principal_YT = tk.Tk()
         self.barra_menu = Menu(self.janela_principal_YT)
         self.arq_menu = Menu(self.barra_menu, tearoff=0)
-        self.arq_menu.add_command(label='Novo', command=self.nada_fazer)
-        self.arq_menu.add_command(label='Abrir', command=self.nada_fazer)
+        self.arq_menu.add_command(label='Novo', command=self.add_link)
+        self.arq_menu.add_command(label='Abrir', command=self.abrir)
         self.arq_menu.add_command(label='Salvar', command=self.nada_fazer)
         self.arq_menu.add_command(label='Salvar como', command=self.nada_fazer)
-        self.arq_menu.add_command(label='Sair', command=self.nada_fazer)
+        self.arq_menu.add_command(label='Fechar', command=self.nada_fazer)
 
-        self.barra_menu.add_separator()
-
-        self.barra_menu.add_command(label='Sair', command=self.janela_principal_YT.quit)
+        self.arq_menu.add_separator()
+        self.arq_menu.add_command(label='Sair', command=self.janela_principal_YT.quit)
         self.barra_menu.add_cascade(label='Arquivo', menu=self.arq_menu)
-        self.
+
+        self.edit_menu = Menu(self.barra_menu, tearoff=0)
+        self.edit_menu.add_command(label='Desfazer')
+
+        self.arq_menu.add_separator()
+        self.edit_menu.add_command(label='Cortar', command=self.nada_fazer)
+        self.edit_menu.add_command(label='Copiar', command=self.nada_fazer)
+        self.edit_menu.add_command(label='Colar', command=self.nada_fazer)
+        self.edit_menu.add_command(label='Delete', command=self.nada_fazer)
+        self.edit_menu.add_command(label='Selecionar tudo', command=self.nada_fazer)
+        self.barra_menu.add_cascade(label='Editar', menu=self.edit_menu)
+
+        self.ajuda_menu = Menu(self.barra_menu, tearoff=0)
+        self.ajuda_menu.add_command(label='Indice de Ajuda', command=self.nada_fazer)
+        self.ajuda_menu.add_command(label='Sobre...', command=self.nada_fazer)
+        self.barra_menu.add_cascade(label='Ajuda', menu=self.ajuda_menu)
 
         self.janela_principal_YT.config(menu=self.barra_menu)
         tk.mainloop()
+
+    def nada_fazer(self):
+        print('teste')
+
+    def abrir(self):
+        print(filedialog.askopenfilename(initialdir='C:/', title='Abrir Arquivo', filetypes=('Todos arquivos', '*.py')))
+
+    def add_link(self):
+        janela_add_link = tk.Tk()
+        frame_1 = tk.Frame(janela_add_link)
+        frame_1.pack()
+        caixa_txt_1 = tk.Entry()
 
 
 iniciando = YouTube_v3()
