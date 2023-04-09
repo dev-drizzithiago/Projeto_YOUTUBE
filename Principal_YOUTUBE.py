c3a27bcb4ea0bf06e7634f3e87a7eb9d0d28a0fe
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-04-09 19:53:58 -0300

    Update Principal_YOUTUBE.py

--- Principal_YOUTUBE.py
+++ Principal_YOUTUBE.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Tk
 from tkinter.ttk import *
 import mysql.connector as db
 
@@ -27,7 +27,7 @@ def janela_principal():
             self.pass_caixa_txt.pack(anchor='center')
 
             self.botao_entrar = tk.Button(self.frame_2, text='Entrar', width=15, height=1, relief='ridge',
-                                          command=self.janela_menu)
+                                          command=self.banco_dados)
             self.botao_entrar.pack(anchor='center')
 
             self.sair_janela = tk.Button(self.janela_principal_YT, text='Sair', width=5, height=1,
@@ -36,16 +36,32 @@ def janela_principal():
 
             tk.mainloop()
 
+        def banco_dados(self):
+            usuario = self.login_caixa_txt.get()
+            senha = self.pass_caixa_txt.get()
+            try:
+                self.conexao_banco = db.connect(host='localhost',
+                                                user=usuario,
+                                                password=senha,
+                                                database='mercadinho_pinheiro')
+                messagebox.showinfo('AVISO!', 'Abrindo o programa \n'
+                                              'Aperte "ok" para continuar!')
+                self.janela_principal_YT.destroy()
+                self.janela_menu()
+            except db.Error as erro:
+                messagebox.showerror('AVISO', F' ==> {erro}')
+
         # JANELA DE MENU
         def janela_menu(self):
-            self.janela_principal_YT.destroy()
             self.janela_menu = tk.Tk()
             self.janela_menu.title('Menu')
             self.janela_menu.geometry('300x150')
             self.frame_menu_1 = tk.Frame(self.janela_menu, width=50, height=50, padx=5, pady=5)
             self.frame_menu_1.pack(fill=tk.Y)
             self.frame_menu_2 = tk.Frame(self.janela_menu, width=50, height=50, padx=5, pady=5)
-            self.frame_menu_2.pack(fill=tk.Y)
+            self.frame_menu_2.pack(fill=tk.Y, expand='yes')
+
+            self.label_frame_1 = tk.LabelFrame(self.janela_menu)
 
             self.opcao_menu = tk.IntVar()
             self.opcao_menu.set(1)
@@ -58,22 +74,22 @@ def janela_principal():
             self.opcao_2 = tk.Radiobutton(self.frame_menu_1, text='Listar os links', padx=5, pady=5,
                                           variable=self.opcao_menu, value=2)
             self.opcao_2.pack(side='left')
-
-            self.botao_enter_menu = tk.Button(self.frame_menu_2, text='Adicionar um link', width=20, height=1, padx=2,
-                                              pady=2, command=self.janela_add_lnk)
+            self.botao_enter_menu = tk.Button(self.frame_menu_2, text='Adicionar um link', width=20, height=1,
+                                              padx=2, pady=2, command=self.janela_add_lnk)
             self.botao_enter_menu.pack(anchor='center')
-            self.botao_voltar_menu = tk.Button(self.frame_menu_2, text='Sair do programa', width=20, height=1, padx=2,
-                                               pady=2, command=self.janela_menu.destroy)
-            self.botao_voltar_menu.pack(side='right')
-
-
+            self.botao_fechar = tk.Button(self.frame_menu_2, text='Sair do programa', width=20, height=1,
+                                          padx=2, pady=2, command=self.janela_menu.quit)
+            self.botao_fechar.pack(side='right')
 
         def janela_add_lnk(self):
+            self.janela_menu.destroy()
             self.janela_add_link = tk.Tk()
+            self.janela_add_link.geometry('400x200')
             self.janela_add_link.title('Adicionando um LINK')
-            self.frame_1 = tk.Frame(self.janela_add_link, width=20, height=20, padx=5, pady=5)
+            self.frame_1 = tk.Frame(self.janela_add_link, padx=5, pady=5)
             self.frame_1.pack(fill=tk.Y)
-            self.frame_2 = tk.Frame(self.janela_add_link, width=20, height=20, padx=5, pady=5)
+
+            self.frame_2 = tk.Frame(self.janela_add_link, padx=5, pady=5)
             self.frame_2.pack(fill=tk.Y)
 
             self.label_txt_1 = tk.Label(self.frame_1, text='Adicione o link', bd=3, padx=10, pady=10)
@@ -82,37 +98,23 @@ def janela_principal():
             self.caixa_txt_1.pack(anchor='center')
 
             self.botao_add_link = tk.Button(self.frame_2, text='Adicionar', bd=4, width=10, height=1, padx=3, pady=3,
-                                       relief='groove', command=self.add_link_db)
+                                            relief='groove', command=self.add_link_db)
             self.botao_add_link.pack(anchor='center')
 
-            self.botao_voltar = tk.Button(self.frame_2, text='Voltar ao Menu Principal', width=6, height=2, pady=2,
-                                        padx=2, command=self.janela_menu)
-            self.botao_voltar.pack(anchor='center')
+            self.botao_voltar = tk.Button(self.frame_2, text='Voltar', bd=4, width=10, height=1, pady=3, padx=3,
+                                          command=self.voltar_menu)
+            self.botao_voltar.pack(anchor='se')
 
-            self.botao_sair_programa = tk.Button(self.frame_2, text='Sair do Programa', width=15, height=2, pady=2,
-                                                 padx=2, command=self.janela_add_link.destroy)
-            self.botao_sair_programa.pack(side='right')
-
-
-        def conexao_db(self):
-            usuario = self.login_caixa_txt.get()
-            senha = self.pass_caixa_txt.get()
-            try:
-                self.conexao_banco = db.connect(host='localhost',
-                                                user=usuario,
-                                                password=senha,
-                                                database='mercadinho_pinheiro')
-                messagebox.showinfo('AVISO!', 'Abrindo o programa \n'
-                                              'Aperte "ok" para continuar!')
-                self.janela_principal_YT.destroy()
-            except db.Error as erro:
-                messagebox.showerror('AVISO', F' ==> {erro}')
+            self.botao_fechar = tk.Button(self.frame_2, text='Sair', bd=4, width=10, height=1, pady=3, padx=3,
+                                          command=self.janela_add_link.destroy)
+            self.botao_fechar.pack(anchor='se')
 
         def add_link_db(self):
             link_yt = str([self.caixa_txt_1.get()])
             titulo_yt_lnk = YouTube(link_yt).title
             print(titulo_yt_lnk)
             cursor = self.conexao_banco.cursor()
+            self.limpar()
             try:
                 comando_SQL = "INSERT INTO youtube (" \
                               "link_youtube, titulo_yt) " \
@@ -124,8 +126,13 @@ def janela_principal():
                 messagebox.showerror('AVISO', f'Ocorreu um erro ao adicionar o link \n'
                                               f'{falha}')
 
+        def limpar(self):
+            self.caixa_txt_1.delete('0', 'end')
+
+        def voltar_menu(self):
+            self.janela_menu.update()
+
     iniciando = YouTube_v3()
 
 
 janela_principal()
-
