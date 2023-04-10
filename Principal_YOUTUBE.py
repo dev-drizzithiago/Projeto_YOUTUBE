6f96837745506a3864cc6e1818caebb36d270235
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-04-10 10:43:58 -0300

    atualizado

--- Principal_YOUTUBE.py
+++ Principal_YOUTUBE.py
@@ -40,7 +40,7 @@ def janela_principal():
             usuario = self.login_caixa_txt.get()
             senha = self.pass_caixa_txt.get()
             try:
-                self.conexao_banco = db.connect(host='db4free.net',
+                self.conexao_banco = db.connect(host='localhost',
                                                 user=usuario,
                                                 password=senha,
                                                 database='drizzithiago_sql')
