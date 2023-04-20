a7965e987b4177e7c0406d9fc6500e4ac32b1d9e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-04-20 16:03:20 -0300

    atualizado

--- Principal_YOUTUBE.py
+++ Principal_YOUTUBE.py
@@ -47,7 +47,7 @@ def janela_principal():
                                                 database='drizzithiago_sql')
                 messagebox.showinfo('AVISO!', 'Abrindo o programa \n'
                                               'Aperte "ok" para continuar!')
-                self.janela_principal_YT.destroy()
+                self.login_banco.destroy()
                 self.janela_menu_tk()
             except db.Error as erro:
                 messagebox.showerror('AVISO', F' ==> {erro}')
@@ -88,27 +88,39 @@ def janela_principal():
         def janela_add_lnk_tk(self):
             self.janela_menu.destroy()
             self.janela_add_link = tk.Tk()
-            self.janela_add_link.geometry('300x300')
+            self.janela_add_link.geometry('800x300')
             self.janela_add_link.title('Adicionando um LINK')
-            self.frame_1 = tk.Frame(self.janela_add_link, padx=5, pady=5)
-            self.frame_1.pack(fill=tk.Y)
-            self.frame_2 = tk.Frame(self.janela_add_link, padx=5, pady=5)
-            self.frame_2.pack(fill=tk.Y)
+            self.frame_1_add_link = tk.Frame(self.janela_add_link, padx=5, pady=5)
+            self.frame_1_add_link.pack(fill=tk.Y)
+            self.frame_2_add_link = tk.Frame(self.janela_add_link, padx=5, pady=5)
+            self.frame_2_add_link.pack(fill=tk.Y)
+            self.frame_3_add_link = tk.Frame(self.janela_add_link, padx=5, pady=5)
+            self.frame_3_add_link.pack(fill=tk.Y)
 
-            self.label_txt_1 = tk.Label(self.frame_1, text='Adicione o link', bd=3, padx=10, pady=10)
+            # Varialvel
+            self._var_titulo_view_add = tk.StringVar()
+
+            # Label caixa de entrada
+            self.label_txt_1 = tk.Label(self.frame_1_add_link, text='Adicione o link', bd=3, padx=10, pady=10)
             self.label_txt_1.pack(side='top')
-            self.caixa_txt_1 = tk.Entry(self.frame_1, textvariable='Caixa de texto', bd=3, width=100)
+            self.caixa_txt_1 = tk.Entry(self.frame_1_add_link, bd=3, width=100)
             self.caixa_txt_1.pack(anchor='center')
 
-            self.botao_add_link = tk.Button(self.frame_2, text='Adicionar', bd=4, width=10, height=1, padx=3, pady=3,
+            # Botões de comando
+            self.botao_add_link = tk.Button(self.frame_2_add_link, text='Adicionar', bd=4, width=10, height=1, padx=3, pady=3,
                                             relief='groove', command=self.add_link_db)
             self.botao_add_link.pack(anchor='center')
-            self.botao_voltar = tk.Button(self.frame_2, text='Voltar', bd=4, width=10, height=1, pady=3, padx=3,
+            self.botao_voltar = tk.Button(self.frame_2_add_link, text='Voltar', bd=4, width=10, height=1, pady=3, padx=3,
                                           relief='groove', command=self.voltar_menu)
             self.botao_voltar.pack(anchor='se')
-            self.botao_fechar = tk.Button(self.frame_2, text='Sair', bd=4, width=10, height=1, pady=3, padx=3,
+            self.botao_fechar = tk.Button(self.frame_2_add_link, text='Sair', bd=4, width=10, height=1, pady=3, padx=3,
                                           relief='groove', command=self.janela_add_link.destroy)
-            self.botao_fechar.pack(side='rigth')
+            self.botao_fechar.pack(anchor='se')
+
+            # Label de visualizador titulo adicionado no banco de dados.
+            self.label_add_titulo = tk.Label(self.frame_3_add_link, textvariable=self._var_titulo_view_add, width=60, height=4,
+                                             padx=5, pady=5, bd=5)
+            self.label_add_titulo.pack(anchor='center')
 
         def janela_view_lnks_tk(self):
             self.janela_view_link = tk.Tk()
