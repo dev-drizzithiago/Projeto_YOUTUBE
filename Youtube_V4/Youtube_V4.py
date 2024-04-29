427f3f53a162a071dc161dfaf49fe2ae8317407d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-29 11:14:20 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -12,11 +12,21 @@ class Youtube_v4:
         self.janela_principal.title('Projeto_YouTube_V4')
         self.janela_principal.resizable(0,0)
 
+        # ##############################################################################################################
         """#### Frames Label's diversos"""
         self.frame_label_principal = Labelframe(self.janela_principal, text='YouTube V4')
         self.frame_label_principal.config(height=495, width=895)
         self.frame_label_principal.pack(fill=tk.BOTH, pady=5, padx=5)
+        # --------------------------------------------------------------------------------------------------------------
+        """#### Frame Lista cache"""
+        self.frame_label_lista_cache = Labelframe(self.frame_label_principal, text='Links Salvos')
+        self.frame_label_lista_cache.place(y=5, x=5)
 
+        # ##############################################################################################################
+        """#### Lista dos links que estão salvos no computador"""
+        self.lista_cache_links_add = tk.Listbox(self.frame_label_lista_cache, selectmode=tk.SINGLE)
+        self.lista_cache_links_add.config(height=5, width=143)
+        self.lista_cache_links_add.pack(anchor='center', fill=tk.BOTH, pady=5, padx=5)
 
         self.caixa_de_entrada_link = Entry(None)
 
