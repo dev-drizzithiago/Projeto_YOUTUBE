06aef4cdadef3c371bd9f90403394ed86e5878e4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-29 10:57:33 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -1,10 +1,21 @@
 import tkinter as tk
 from tkinter.ttk import *
+from threading import Thread
 
 
 class Youtube_v4:
     def __init__(self):
+
+        """ Janela principal"""
         self.janela_principal = tk.Tk()
+        self.janela_principal.geometry('900x500+250+100')
+        self.janela_principal.title('Projeto_YouTube_V4')
+        self.janela_principal.resizable(0,0)
+
+
+
+        """#### Declarações de variaveis"""
+
         self.janela_principal.mainloop()
 
 
