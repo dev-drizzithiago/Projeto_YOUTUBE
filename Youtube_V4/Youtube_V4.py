f30318eee24449b5a023d40341e690a6bd81b711
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-29 11:21:42 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -25,14 +25,20 @@ class Youtube_v4:
         # ##############################################################################################################
         """#### Lista dos links que estão salvos no computador"""
         self.lista_cache_links_add = tk.Listbox(self.frame_label_lista_cache, selectmode=tk.SINGLE)
-        self.lista_cache_links_add.config(height=5, width=143)
+        self.lista_cache_links_add.config(height=5, width=140)
         self.lista_cache_links_add.pack(anchor='center', fill=tk.BOTH, pady=5, padx=5)
+        # --------------------------------------------------------------------------------------------------------------
+        """#### Barra de rolagem da lista de cache"""
+        self.barra_rolagem_lista_cache_Y = Scrollbar(self.frame_label_lista_cache, orient=tk.VERTICAL)
+        self.barra_rolagem_lista_cache_Y.place(in_=self.barra_rolagem_lista_cache_Y)
+        self.barra_rolagem_lista_cache_Y.place(relx=1, relheight=1, bordermode='outside')
+        self.barra_rolagem_lista_cache_Y.config(command=self.lista_cache_links_add.yview)
+        self.lista_cache_links_add.config(yscrollcommand=self.barra_rolagem_lista_cache_Y.set)
 
         self.caixa_de_entrada_link = Entry(None)
 
-
-
         """#### Declarações de variaveis"""
+        self.ativar_ = False
 
         self.janela_principal.mainloop()
 
