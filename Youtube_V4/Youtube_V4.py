f78a88d7a07e1fbe39e4fe6fb7bc4f50a8768f07
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-29 11:02:56 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -12,6 +12,14 @@ class Youtube_v4:
         self.janela_principal.title('Projeto_YouTube_V4')
         self.janela_principal.resizable(0,0)
 
+        """#### Frames Label's diversos"""
+        self.frame_label_principal = Labelframe(self.janela_principal, text='YouTube V4')
+        self.frame_label_principal.config(height=495, width=895)
+        self.frame_label_principal.pack(fill=tk.BOTH, pady=5, padx=5)
+
+
+        self.caixa_de_entrada_link = Entry(None)
+
 
 
         """#### Declarações de variaveis"""
