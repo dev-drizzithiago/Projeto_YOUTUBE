f7e6450a801f5d1a1b3b3652eb4efb1733c6beb6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-30 11:51:39 -0300

    Update Youtube_V1.py

--- Youtube_V1/Youtube_V1.py
+++ Youtube_V1/Youtube_V1.py
@@ -2,9 +2,14 @@ from threading import Thread
 
 lista_menu = ['Adicionar link', 'Baixar links', 'Ver links', 'Sair']
 
+# ######################################################################################################################
+"""#### Funções basicas"""
+def LeiaInt(valor_recebido):
+    pass
+
+
+# ######################################################################################################################
 """#### Funções de threads"""
-
-
 def thread_adicionar_link():
     print('thread_adicionar_link')
     pass
@@ -19,10 +24,8 @@ def thread_ver_links():
     print('thread_ver_links')
     pass
 
-
+# ######################################################################################################################
 """#### Funções de processo"""
-
-
 def adicionar_link():
     print('adicionar_link')
     pass
@@ -37,7 +40,12 @@ def ver_links():
     print('ver_links')
     pass
 
+# ######################################################################################################################
 """#### Menu principal"""
 def menu():
     for indice in range(len(lista_menu)):
         print(f'[{indice + 1}] - {lista_menu[indice]}')
+
+    opc_menu_principal = leiaInt('Escolha uma opcão: ')
+
+menu()
