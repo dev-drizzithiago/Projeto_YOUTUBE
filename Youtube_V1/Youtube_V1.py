4242a2fae1b1c33291e8b57bcd14859c8b2640dc
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-30 10:35:09 -0300

    Update Youtube_V1.py

--- Youtube_V1/Youtube_V1.py
+++ Youtube_V1/Youtube_V1.py
@@ -1,5 +1,6 @@
 
 lista_menu = ['Adicionar link', 'Baixar links', 'Ver links', 'Sair']
 
-for indice in lista_menu:
-    print(f'[{indice}]{lista_menu[indice]}')
+for indice in range(len(lista_menu)):
+    print(f'[{indice}] - {lista_menu[indice]}')
+    
