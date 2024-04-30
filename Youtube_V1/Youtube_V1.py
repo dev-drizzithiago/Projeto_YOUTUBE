1c9fb1d677fbd3677c930f71fd4bdcd42f0fa6fe
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-30 11:45:42 -0300

    Update Youtube_V1.py

--- Youtube_V1/Youtube_V1.py
+++ Youtube_V1/Youtube_V1.py
@@ -1,5 +1,28 @@
 
 lista_menu = ['Adicionar link', 'Baixar links', 'Ver links', 'Sair']
 
+"""#### Funções de threads"""
+def thread_adicionar_link():
+    pass
+
+def thread_baixar_links():
+    pass
+
+def thread_ver_links():
+    pass
+
+
+"""#### Funções de processo"""
+def adicionar_link():
+    pass
+;;
+def baixar_links()
+    pass
+
+def ver_links():
+    pass
+
+
 for indice in range(len(lista_menu)):
-    print(f'[{indice}] - {lista_menu[indice]}')
+    print(f'[{indice + 1}] - {lista_menu[indice]}')
+
