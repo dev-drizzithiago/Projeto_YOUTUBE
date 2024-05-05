51b6c39033e431c4968d9eafb63a59022fe4e240
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-05-04 23:06:40 -0300

    Update Youtube_V2.py

--- Youtube_V2/Youtube_V2.py
+++ Youtube_V2/Youtube_V2.py
@@ -1,9 +1,10 @@
 from pytube import YouTube
 
-lista_menu_principal = []
+lista_menu_principal = ['Adicionar link', 'Downloads', 'Links adicionados', 'Abrir arquivo', 'Sair']
 lista_menu_downloads = []
 
 
+"""#### Função simples"""
 def logo_tube(valor_entrada):
     linhas = '---' * 10
     print(f'{linhas}{valor_entrada}{linhas}')
@@ -16,6 +17,9 @@ def leiaInt(valor_entrada):
         except TypeError:
             print(f'Você digitou um valor errado!')
 
+"""#### Funções threads"""
+def thread_adicionar_link 
+
 
 """#### Menu principal"""
 def menu_principal():
@@ -24,5 +28,12 @@ def menu_principal():
     print('                                          @drizzithiago ')
     print('----' * 24)
 
+    logo_tube('Menu Principal')
     for valor_menu in range(len(lista_menu_principal)):
+        print(f'{valor_menu + 1} ==> {lista_menu_principal[valor_menu]}')
 
+    valor_opc = leiaInt('Escolha uma opção: ')
+
+
+
+menu_principal()
