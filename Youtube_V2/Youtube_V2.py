e012c06349aecdd30a299939456d82246df80d39
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-05-06 16:36:00 -0300

    Update Youtube_V2.py

--- Youtube_V2/Youtube_V2.py
+++ Youtube_V2/Youtube_V2.py
@@ -107,9 +107,9 @@ def view_links_add():
         abrindo_arq_tube = valor_arq_tube.readlines()
 
         for valor_link in abrindo_arq_tube:
-            obj_tube_titulo = YouTube(valor_link)
+            obj_tube_titulo = YouTube(valor_link).title
             obj_tube_link = valor_link
-            print(f'[{obj_tube_titulo.title}] \n{obj_tube_link}')
+            print(f'[{obj_tube_titulo}] \n{obj_tube_link}')
 
     except FileNotFoundError:
         print(linha)
