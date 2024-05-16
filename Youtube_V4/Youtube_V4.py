54004ce1ba1a8eaea7f0c49414839f10e372eae3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-05-15 21:31:27 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -111,11 +111,11 @@ class Youtube_v4:
 
         self.var_radio_ = tk.IntVar()
         self.radio_mp3_midia = Radiobutton(self.frame_lbl_botao_radio_opc_midia, text='Downloads (MP3)')
-        self.radio_mp3_midia.config(textvariable=self.var_radio_.get, value=1)
+        self.radio_mp3_midia.config(variable=self.var_radio_.get, value=1)
         self.radio_mp3_midia.place(y=5, x=50)
 
         self.radio_mp4_midia = Radiobutton(self.frame_lbl_botao_radio_opc_midia, text='Downloads (MP4)')
-        self.radio_mp4_midia.config(textvariable=self.var_radio_.get, value=2)
+        self.radio_mp4_midia.config(variable=self.var_radio_.get, value=2)
         self.radio_mp4_midia.place(y=25, x=50)
         # --------------------------------------------------------------------------------------------------------------
 
