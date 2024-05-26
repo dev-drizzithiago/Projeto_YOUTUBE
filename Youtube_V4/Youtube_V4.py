3634b4b67c3e6b7d8c2895da00bc5c1f6ac43d10
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-05-26 18:01:53 -0300

    Update Youtube_V4.py

--- Youtube_V4/Youtube_V4.py
+++ Youtube_V4/Youtube_V4.py
@@ -279,8 +279,7 @@ class Youtube_v4:
         self.frame_lbl_botao_limpar.config(text='Limpar')
         self.botao_limpar_lista.config(command=self.thread_limpar_lista_cache)
 
-        self.func_time_10seg()
-        self.lbl_status_processos.config(text=f'Stand by!')
+        self.thread_func_time_10seg()
 
     """# Funções básicas"""
     def func_time_10seg(self):
