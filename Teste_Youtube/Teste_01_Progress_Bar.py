6bea90a595c7d2885a2a475ab6b78e0b364c0ea2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-04-26 20:23:24 -0300

    Update Teste_01_Progress_Bar.py

--- Teste_Youtube/Teste_01_Progress_Bar.py
+++ Teste_Youtube/Teste_01_Progress_Bar.py
@@ -4,7 +4,7 @@ import os
 link = 'https://www.youtube.com/watch?v=RHvM6vgCXhk&list=RDRHvM6vgCXhk&start_radio=1&ab_channel=zahoz75'
 download = YouTube(link)
 
-ys = download.streams.get_highest_resolution()
+ys = download.streams.get_audio_only()
 
 caminho_download = os.path.join(os.path.dirname(__file__))
 print(caminho_download)
