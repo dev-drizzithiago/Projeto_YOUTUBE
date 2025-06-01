d960c1ade6bfd809a19ba96082c7e97b6bf503b5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-01 17:29:35 -0300

    Update main.py

--- Youtube_V5/main.py
+++ Youtube_V5/main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
 app = QApplication(sys.argv)
 
 class JanelaPrincipalDownYT:
@@ -10,8 +10,13 @@ class JanelaPrincipalDownYT:
         self.janela_principal = QWidget()
         self.janela_principal.show()
 
+        self.lbl_titulo_link = QLabel(self.janela_principal)
+        self.lbl_titulo_link.show()
+
     def download_link(self, link: str):
         self._link = link
+        self.lbl_titulo_link(self._link)
+
 
 obj_init_yt = JanelaPrincipalDownYT()
 resultado = obj_init_yt.download_link('link')
